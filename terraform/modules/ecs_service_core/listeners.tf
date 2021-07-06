6835a7b4beeeec3f234b119cb5f3ab77912b462f
Mateo Gómez-Zuluaga <mateo.goemz@data-tropics.com>
2021-07-06 07:18:58 -0500

    ADding alb support for listener

--- terraform/modules/ecs_service_core/listeners.tf
+++ terraform/modules/ecs_service_core/listeners.tf
@@ -2,8 +2,8 @@ resource "aws_lb_listener" "default" {
   load_balancer_arn = var.aws_lb_arn
   port              = var.alb_listener_port
   protocol          = var.alb_listener_protocol
-  ssl_policy        = var.ssl_policy
-  certificate_arn   = var.certificate_arn
+  ssl_policy        = var.load_balancer_type == "application" ? null : var.ssl_policy
+  certificate_arn   = var.load_balancer_type == "application" ? null : var.certificate_arn
 
   default_action {
     type             = "forward"
